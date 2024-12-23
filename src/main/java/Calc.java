bccc0a174033bc221a2a68df7e98090fb421513b
asungkim <asungkim007@naver.com>
2024-12-23 12:32:51 +0900

    기능 구현 완료

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -1,7 +1,7 @@
 public class Calc {
     public static int run(String s) {
 
-        // 3 + 5 -> 3+5
+        // (3+5) * 5
         s=s.replace(" ","");
         return execute(s);
     }
@@ -11,16 +11,49 @@ public class Calc {
             return run(s.substring(1,s.length()-1));
         }
 
-        for (int i = 0; i < s.length(); i++) {
-            char c=s.charAt(i);
-            if (c=='+'||c=='-'||c=='*'||c=='/') {
-                int left=Integer.parseInt(s.substring(0,i));
-                int right=Integer.parseInt(s.substring(i+1));
-                return calculate(left,right,c);
-            }
+        int idx=findLowerPriority(s);
+        if (idx != -1) {
+            char op=s.charAt(idx);
+            int left=execute(s.substring(0,idx));
+            int right=execute(s.substring(idx+1));
+
+            return calculate(left,right,op);
         }
 
         return Integer.parseInt(s);
+    }
+
+
+    // (3+5)*5+-4
+    private static int findLowerPriority(String s) {
+        int tmp=0;
+        int min=Integer.MAX_VALUE;
+        int minIdx=-1;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c=s.charAt(i);
+            if (c=='(') tmp++;
+            else if (c==')') tmp--;
+            else if (tmp==0&&(c=='+'||c=='-'||c=='*'||c=='/')) {
+                if (c=='-' &&(i==0||s.charAt(i-1)=='(')) {
+                    continue;
+                }
+                int level = level(c);
+                if (level<min) {
+                    min=level;
+                    minIdx=i;
+                }
+            }
+        }
+
+        return minIdx;
+    }
+
+    private static int level(char c) {
+        if (c=='+'||c=='-') {
+            return 1;
+        }
+        else return 2;
     }
 
     private static int calculate(int left, int right, char operator) {
